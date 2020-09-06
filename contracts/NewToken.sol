5bdc5e9c48c12474658ac381dce01f3ac74f1c5a
Wan Hao Jun <haojun@wtech.software>
2020-09-06 19:48:37 +0800

    2. Transfer Tokens

--- contracts/NewToken.sol
+++ contracts/NewToken.sol
@@ -1,12 +1,38 @@
 pragma solidity ^0.5.0;
 
 contract NewToken {
-    // Constructor
-    // Set the total number of tokens
-    // Read the total number of tokens
+    string public name = "New Token";
+    string public symbol = "N3w";
+    string public standard = "New Token v1.0";
     uint256 public totalSupply;
 
-    constructor() public {
-        totalSupply = 1000000;
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+
+    mapping(address => uint256) public balanceOf;
+
+    // Constructor
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+        // allocate the initial supply
+    }
+
+    // Transfer
+    function transfer(address _to, uint256 _value)
+        public
+        returns (bool success)
+    {
+        // Exception if account doesn't have enough
+        require(balanceOf[msg.sender] >= _value);
+
+        // Transfer the balance
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        // Transfer Event
+        emit Transfer(msg.sender, _to, _value);
+
+        // Return boolean
+        return true;
     }
 }
