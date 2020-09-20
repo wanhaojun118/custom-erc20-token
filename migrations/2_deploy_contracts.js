c623b57ef8c5a51f909c8d15ab09135d4a33b773
Wan Hao Jun <haojun@wtech.software>
2020-09-20 16:52:45 +0800

    4. Token Sale

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,12 @@
 const NewToken = artifacts.require("./NewToken.sol"); // reading token contract and assign to variable
+const NewTokenSale = artifacts.require("./NewTokenSale.sol"); // reading token sale contract
 
 module.exports = function (deployer) {
-  deployer.deploy(NewToken, 1000000); // deploy new token's contract
+  // deploy token contract
+  deployer.deploy(NewToken, 1000000).then(function () {
+    const tokenPrice = 1000000000000000; // 0.001 Ether
+
+    // deploy token sale contract using token contract's address
+    return deployer.deploy(NewTokenSale, NewToken.address, tokenPrice);
+  });
 };
