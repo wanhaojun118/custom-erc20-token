df71e0831caf3f29788a4efe89937737658834e6
Wan Hao Jun <haojun@wtech.software>
2020-09-28 22:47:07 +0800

    (1) Get user balance and show token price in Eth (2) Add token sale function (3) Listen to token "Sell" event

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ App = {
     web3Provider: null,
     contracts: {},
     account: 0x0,
+    loading: false,
+    tokenPrice: 0,
+    tokenSold: 0,
+    tokensAvailable: 750000,
     init: function () {
         console.log("App initialized...");
         return App.initWeb3();
@@ -36,19 +40,95 @@ App = {
                     console.log("Token contract's address: ", newToken.address);
                 });
 
+                App.listenForEvents();  // Always listen to "Sell" event
                 return App.render();
             });
         });
     },
 
+    // Listen to "Sell" event(s) emitted from contract
+    listenForEvents: function () {
+        App.contracts.NewTokenSale.deployed().then(function (instance) {
+            instance.Sell({}, {
+                fromBlock: 0,
+                toBlock: "latest",
+            }).watch(function (error, event) {
+                console.log("Event triggered", event);
+                App.render();
+            });
+        });
+    },
+
     render: function () {
+        if (App.loading) {
+            return;
+        }
+        App.loading = true;
+
+        const loader = $("#loader");
+        const content = $("#content");
+        loader.show();
+        content.hide();
+
         // Load account data
         web3.eth.getCoinbase(function (err, account) {
             if (!err) {
                 App.account = account;
                 $("#account").html(App.account);
             }
-        })
+        });
+
+        // Load token sale contract
+        App.contracts.NewTokenSale.deployed().then(function (instance) {
+            newTokenSaleInstance = instance;
+            return newTokenSaleInstance.tokenPrice();
+        }).then(function (tokenPrice) {
+            App.tokenPrice = tokenPrice;
+            $("#token-price").html(web3.fromWei(App.tokenPrice.toNumber(), "ether"));
+
+            return newTokenSaleInstance.tokenSold();
+        }).then(function (tokenSold) {
+            App.tokenSold = tokenSold.toNumber();
+            $("#token-sold").html(App.tokenSold);
+            $("#token-available").html(App.tokensAvailable);
+
+            const progressPercent = (App.tokenSold / App.tokensAvailable) * 100;
+            $("#progress").css("width", progressPercent + "%");
+
+            // Load token contract
+            App.contracts.NewToken.deployed().then(function (instance) {
+                newTokenInstance = instance;
+                return newTokenInstance.balanceOf(App.account);
+            }).then(function (balance) {
+                App.balance = balance.toNumber();
+                $("#token-balance").html(App.balance);
+
+                App.loading = false;
+
+                loader.hide();
+                content.show();
+            });
+        });
+    },
+
+    buyTokens: function () {
+        $("#content").hide();
+        $("#loader").show();
+
+        const numberOfToken = $("#number-of-token").val();
+
+        App.contracts.NewTokenSale.deployed().then(function (instance) {
+            return instance.buyTokens(numberOfToken, {
+                from: App.account,
+                value: numberOfToken * App.tokenPrice,
+                gas: 500000
+            });
+        }).then(function (result) {
+            console.log("Tokens bought...");
+            $("form").trigger("reset"); // reset number of tokens in form
+
+            // Wait for Sell event to triggered a re-render
+        });
     }
 }
 
