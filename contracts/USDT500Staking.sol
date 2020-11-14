cec973cdcd7ff41bc6cb76fe84b89c355c0d0b6a
Wan Hao Jun <haojun@wtech.software>
2020-11-14 17:39:46 +0800

    (1) Redeploy Minion contract (2) Redeploy ETH500Staking contract (3) Deploy USDT token contract (4) Deploy USDT500Staking contract

--- contracts/USDT500Staking.sol
+++ contracts/USDT500Staking.sol
@@ -3,24 +3,25 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 
-contract ETH500Staking {
+contract USDT500Staking {
     using SafeERC20 for IERC20;
     using SafeMath for uint256;
-
+    
+    IERC20 public USDTInstance;
     IERC20 public MinionInstance;
     address[] internal stakeholders;
     mapping(address => uint256) internal stakes; // Stakeholder's stake(s)
     uint256 public totalStakes;
-
+    
     event AddStake(address sender, uint256 amount);
     event WithdrawStake(address receiver, uint256 amount);
     event HarvestInterest(address stakeholder, uint256 amount);
-
-    constructor(address _MinionAddress) public {
-        MinionInstance = ERC20(_MinionAddress); // Assign minion contract
+    
+    constructor(address _USDTAddress, address _MinionAddress) public {
+        USDTInstance = ERC20(_USDTAddress);
+        MinionInstance = ERC20(_MinionAddress);
     }
-
-    // To check if address is one of the stakeholders
+    
     function isStakeholder(address _address)
         internal
         view
@@ -31,14 +32,12 @@ contract ETH500Staking {
         }
         return (false, 0);
     }
-
-    // To add stakeholder if @param _stakeholder is not one
+    
     function addStakeholder(address _stakeholder) internal {
         (bool _isStakeholder, ) = isStakeholder(_stakeholder);
         if (!_isStakeholder) stakeholders.push(_stakeholder);
     }
-
-    // To remove a stakeholder if @param _stakeholder is one
+    
     function removeStakeholder(address _stakeholder) internal {
         (bool _isStakeholder, uint256 s) = isStakeholder(_stakeholder);
         if (_isStakeholder) {
@@ -49,8 +48,7 @@ contract ETH500Staking {
             stakeholders.pop();
         }
     }
-
-    // Get stakeholder list
+    
     function getStakeholders()
         public
         view
@@ -59,38 +57,41 @@ contract ETH500Staking {
         _stakeholders = stakeholders;
         return _stakeholders;
     }
-
-    // To retrieve the stake of specific stakeholder
+    
     function stakeOf(address _stakeholder) public view returns (uint256) {
         return stakes[_stakeholder];
     }
-
-    // For user to deposit stake
-    function addStake() public payable {
-        require(msg.value > 0, "Stake amount cannot be 0 when adding stake");
-
-        if (stakes[msg.sender] == 0) {
-            stakes[msg.sender] = msg.value; // Create new stake for sender
+    
+    function addStake(address _stakeholder, uint256 _amount) public returns (bool _success) {
+        _success = (_amount > 0);
+        require(_success, "Stake amount cannot be 0 when adding stake");
+        
+        _success = (USDTInstance.transferFrom(_stakeholder, address(this), _amount));
+        require(_success, "Stake amount not being credited to contract balance");
+        
+        if (stakes[_stakeholder] == 0) {
+            stakes[_stakeholder] =_amount; // Create new stake for sender
         }else{
-            stakes[msg.sender] = SafeMath.add(stakes[msg.sender], msg.value); // Add amount to existing stake
+            stakes[_stakeholder] = SafeMath.add(stakes[_stakeholder], _amount); // Add amount to existing stake
         }
-
-        totalStakes = SafeMath.add(totalStakes, msg.value); // Add stake amount to total stakes
-
-        addStakeholder(msg.sender); // Add stakeholder to stakeholders list
-
-        emit AddStake(msg.sender, msg.value);
+        
+        totalStakes = SafeMath.add(totalStakes, _amount);
+        
+        addStakeholder(_stakeholder);
+        
+        emit AddStake(_stakeholder, _amount);
+        
+        return _success;
     }
-
-    // To withdraw stakeholder's stake in ETH
-    function withdrawStake(address payable _stakeholder, uint256 _amount)
-        public
-        returns (bool _success)
+    
+    function withdrawStake(address _stakeholder, uint256 _amount)
+        public returns (bool _success)
     {
         _success = (stakes[_stakeholder] >= _amount);
         require(_success, "Insufficient stake on stake withdrawal");
-
-        _stakeholder.transfer(_amount);
+        
+        _success = (USDTInstance.transfer(_stakeholder, _amount));
+        require(_success, "Amount not being credited to stakeholder's balance");
 
         stakes[_stakeholder] = SafeMath.sub(stakes[_stakeholder], _amount);
 
@@ -101,25 +102,24 @@ contract ETH500Staking {
         }
 
         emit WithdrawStake(_stakeholder, _amount);
-
+        
         return _success;
     }
-
-    // To harvest interest
+    
     function harvestInterest(address _stakeholder, uint256 _amount) public returns (bool _success) {
         uint256 _balanceOfContract = MinionInstance.balanceOf(address(this));
         _success = (_balanceOfContract >= _amount);
-        require(_success, "Insufficient amount in ETH500 staking pool");
+        require(_success, "Insufficient balance in USDT500 staking pool");
 
         _success = (_amount > 0);
-        require(_success, "Invalid amount on harvesting interest");
+        require(_success, "Cannot harvest 0 USDT");
 
         (bool _isStakeholder, uint256 _s) = isStakeholder(_stakeholder);
         _success = _isStakeholder;
         require(_success, "Requester is not our stakeholder");
 
         _success = MinionInstance.transfer(_stakeholder, _amount);
-        require(_success, "Transfer failed on Minion contract");
+        require(_success, "Transfer failed on USDT contract");
 
         emit HarvestInterest(_stakeholder, _amount);
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,11 @@
 const Minion = artifacts.require("./Minion.sol");
+const USDT = artifacts.require("./USDT.sol");
 const ETH500Staking = artifacts.require("./ETH500Staking.sol");
+const USDT500Staking = artifacts.require("./USDT500Staking.sol");
 
 module.exports = async function (deployer) {
   await deployer.deploy(Minion);
+  await deployer.deploy(USDT);
   await deployer.deploy(ETH500Staking, Minion.address);
+  await deployer.deploy(USDT500Staking, USDT.address, Minion.address);
 };
